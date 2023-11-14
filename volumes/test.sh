3cd59ae895e4d3ca06052f0337fec3064830034e
PainIsWhenSomeBodyDropT3 <thats_change_everything@proton.me>
2023-11-14 17:35:10 +0300

    reinit all module

---
+++ volumes/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "deb http://archive.debian.org/debian/ stretch main contrib non-free
+deb http://archive.debian.org/debian-security/ stretch/updates main contrib non-free" >/etc/apt/sources.list
