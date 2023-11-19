bd8c9080cc991f2f9bd7d45ecf50e6337a765f67
PainIsWhenSomeBodyDropT3 <thats_change_everything@proton.me>
2023-11-19 22:12:49 +0300

    add weave practice

---
+++ using-weawe/install-weawe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo curl -sL git.io/weave -o /usr/local/bin/weave
+sudo chmod a+x /usr/local/bin/weave
---
+++ using-weawe/start-vms.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker-machine create -d virtualbox weawe-redis
+docker-machine create -d virtualbox weawe-identidock
